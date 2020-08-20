0d8cdeab664467f299b8ccb3b9cb9d075dbd1f60
Walter Lapina <47811260+sugarfr33@users.noreply.github.com>
2020-08-21 00:58:51 +0800

    Add files via upload

---
+++ captiveportal-customjs.js
@@ -0,0 +1,29 @@
+$('#rate').append(
+    '<h6><span class="badge badge-primary">05 Pesos</span> 1 Hour</h6>' +
+    '<h6><span class="badge badge-secondary">10 Pesos</span> 2 Hours</h6>' +
+    '<h6><span class="badge badge-success">15 Pesos</span> 4 Hours</h6>' +
+    '<h6><span class="badge badge-danger">20 Pesos</span> 5 Hours</h6>' +
+    '<h6><span class="badge badge-info">30 Pesos</span> 1 Day</h6>'
+);
+
+$('#carousel_image').append(
+    '<div class="carousel-item active">' +
+    '    <img class="d-block w-100" src="captiveportal-1.jpg" alt="First slide">' +
+    '</div>' +
+    '<div class="carousel-item">' +
+    '    <img class="d-block w-100" src="captiveportal-2.jpg" alt="Second slide">' +
+    '</div>' +
+    '<div class="carousel-item">' +
+    '    <img class="d-block w-100" src="captiveportal-3.jpg" alt="Third slide">' +
+    '</div>' +
+    '<div class="carousel-item">' +
+    '    <img class="d-block w-100" src="captiveportal-4.jpg" alt="Fourth slide">' +
+    '</div>'
+);
+
+$('#carousel_indicator').append(
+    '<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>'+
+    '<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>'+
+    '<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>'+
+    '<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>'
+);
